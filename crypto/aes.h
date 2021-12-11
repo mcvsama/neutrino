485f1b161ef83c617bf2ff62464a7af67d678e54
Michał Gawron <michal@gawron.name>
2021-12-11 11:58:10 +0100

    Add AES encryption functions

---
+++ crypto/aes.h
@@ -0,0 +1,93 @@
+/* vim:ts=4
+ *
+ * Copyleft 2012…2017  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef NEUTRINO__CRYPTO__AES_H__INCLUDED
+#define NEUTRINO__CRYPTO__AES_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+
+// Lib:
+#include <cryptopp/aes.h>
+
+// Boost:
+#include <boost/endian/conversion.hpp>
+
+// Neutrino:
+#include <neutrino/blob.h>
+#include <neutrino/exception.h>
+
+
+namespace neutrino {
+
+struct AES_Args
+{
+	BlobView	data;
+	BlobView	key;
+};
+
+
+struct CTR_128_Args
+{
+	BlobView	data;
+	BlobView	key;
+	BlobView	nonce;
+};
+
+
+inline Blob
+aes_encrypt (AES_Args const& args)
+{
+	if (args.data.size() != CryptoPP::AES::BLOCKSIZE)
+		throw std::runtime_error ("invalid data block size " + std::to_string (args.data.size()) + " B, must be " + std::to_string (CryptoPP::AES::BLOCKSIZE) + " B");
+
+	if (args.key.size() < CryptoPP::AES::MIN_KEYLENGTH || args.key.size() > CryptoPP::AES::MAX_KEYLENGTH)
+		throw std::runtime_error ("invalid key block size " + std::to_string (args.key.size()) + " B, must be [" +
+								  std::to_string (CryptoPP::AES::MIN_KEYLENGTH) + ".." + std::to_string (CryptoPP::AES::MAX_KEYLENGTH) + "] B");
+
+	Blob result;
+	result.resize (CryptoPP::AES::BLOCKSIZE);
+
+	CryptoPP::AESEncryption enc;
+	enc.SetKey (args.key.data(), args.key.size());
+	enc.ProcessBlock (args.data.data(), result.data());
+
+	return result;
+}
+
+
+inline Blob
+aes_ctr_xor (CTR_128_Args const& args)
+{
+	Blob result = Blob (args.data);
+	Blob key_stream;
+
+	// Key stream needs to be at least the size of input data:
+	for (uint64_t i = 0; key_stream.size() < result.size(); ++i)
+	{
+		uint64_t big_i = boost::endian::native_to_big (i);
+		Blob counter (sizeof (big_i), 0x00);
+		std::memcpy (counter.data(), &big_i, sizeof (big_i));
+		key_stream += aes_encrypt ({ .data = Blob (args.nonce) + counter, .key = args.key });
+	}
+
+	for (size_t i = 0; i < result.size(); ++i)
+		result[i] ^= key_stream[i];
+
+	return result;
+}
+
+} // namespace neutrino
+
+#endif
+
---
+++ crypto/tests/aes.test.cc
@@ -0,0 +1,48 @@
+/* vim:ts=4
+ *
+ * Copyleft 2008…2013  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+// Standard:
+#include <cstddef>
+
+// Neutrino:
+#include <neutrino/test/auto_test.h>
+#include <neutrino/crypto/aes.h>
+#include <neutrino/string.h>
+
+
+namespace neutrino::test {
+namespace {
+
+AutoTest t1 ("neutrino::aes_ctr_xor()", []{
+	Blob data1 ({ 0x00, 0x01, 0x05, 0x10, 0x12, 0x15, 0x00, 0xf0, 0xfa, 0x2f, 0x3f, 0x55, 0x6f, 0x8a, 0xca, 0xd0 });
+	Blob encrypted1 = aes_ctr_xor ({
+		.data = data1,
+		.key = data1,
+		.nonce = Blob (8, 0x00),
+	});
+
+	test_asserts::verify ("calculated encrypted result (1) is correct", to_hex_string (encrypted1) == "da80702714e2170902465d3db3405cda");
+
+	Blob data2 ({ 0x00, 0x01, 0x05, 0x10, 0x12, 0x15, 0x00, 0xf0, 0xfa, 0x2f, 0x3f, 0x55, 0x6f, 0x8a, 0xca, 0xd0, 0xfa, 0xdf, 0xca, 0xf8, 0x55, 0x66, 0x99, 0xaa });
+	Blob encrypted2 = aes_ctr_xor ({
+		.data = data2,
+		.key = data1,
+		.nonce = Blob (8, 0x00),
+	});
+
+	test_asserts::verify ("calculated encrypted result (2) is correct", to_hex_string (encrypted2) == "da80702714e2170902465d3db3405cda2f1a8f4bd065952b");
+});
+
+} // namespace
+} // namespace neutrino::test
+
