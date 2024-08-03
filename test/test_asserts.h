b91a586e718eb8838c838013f4ba7ff30ff86cc7
Michał Gawron <michal@gawron.name>
2024-08-03 22:31:01 +0200

    Better name for test_asserts() description: expectation

--- test/test_asserts.h
+++ test/test_asserts.h
@@ -32,7 +32,7 @@ namespace detail {
 
 template<class T1, class T2, class T3>
 	inline void
-	verify_equal_with_epsilon_scalar (std::string const& test_explanation, T1 const& value1, T2 const& value2, T3 const& epsilon)
+	verify_equal_with_epsilon_scalar (std::string const& test_expectation, T1 const& value1, T2 const& value2, T3 const& epsilon)
 	{
 		using std::isfinite;
 
@@ -40,7 +40,7 @@ template<class T1, class T2, class T3>
 		{
 			using std::to_string;
 
-			throw TestAssertFailed (test_explanation, "actual value " + to_string (value1) + " not equal to expected " +
+			throw TestAssertFailed (test_expectation, "actual value " + to_string (value1) + " not equal to expected " +
 									to_string (value2) + " with epsilon " +
 									to_string (epsilon) + "; diff=" +
 									to_string (value2 - value1));
@@ -50,7 +50,7 @@ template<class T1, class T2, class T3>
 
 template<class T1, class T2, class T3>
 	inline void
-	verify_equal_with_epsilon_vector (std::string const& test_explanation, T1 const& value1, T2 const& value2, T3 const& epsilon)
+	verify_equal_with_epsilon_vector (std::string const& test_expectation, T1 const& value1, T2 const& value2, T3 const& epsilon)
 	{
 		using std::isfinite;
 
@@ -59,7 +59,7 @@ template<class T1, class T2, class T3>
 			using std::to_string;
 			using debug::to_string;
 
-			throw TestAssertFailed (test_explanation, "actual value " + to_string (value1) + " not equal to expected " +
+			throw TestAssertFailed (test_expectation, "actual value " + to_string (value1) + " not equal to expected " +
 									to_string (value2) + " with epsilon " +
 									to_string (epsilon) + "; abs diff=" +
 									to_string (abs (value2 - value1)));
@@ -69,7 +69,7 @@ template<class T1, class T2, class T3>
 
 template<class T1, class T2, class T3>
 	inline void
-	verify_equal_with_epsilon_matrix (std::string const& test_explanation, T1 const& value1, T2 const& value2, T3 const& epsilon)
+	verify_equal_with_epsilon_matrix (std::string const& test_expectation, T1 const& value1, T2 const& value2, T3 const& epsilon)
 	{
 		using std::isfinite;
 
@@ -78,7 +78,7 @@ template<class T1, class T2, class T3>
 			using std::to_string;
 			using debug::to_string;
 
-			throw TestAssertFailed (test_explanation, "actual value " + to_string (value1) + " not equal to expected " +
+			throw TestAssertFailed (test_expectation, "actual value " + to_string (value1) + " not equal to expected " +
 									to_string (value2) + " with epsilon " +
 									to_string (epsilon) + "; euclidean_norm diff=" +
 									to_string (euclidean_norm (value2 - value1)));
@@ -98,26 +98,26 @@ template<class T>
 
 
 inline void
-verify (std::string const& test_explanation, bool condition)
+verify (std::string const& test_expectation, bool condition)
 {
 	if (!condition)
-		throw TestAssertFailed (test_explanation, "condition failed");
+		throw TestAssertFailed (test_expectation, "condition failed");
 }
 
 
 template<class T1, class T2, class T3>
 	inline void
-	verify_equal_with_epsilon (std::string const& test_explanation, T1 const& value1, T2 const& value2, T3 const& epsilon)
+	verify_equal_with_epsilon (std::string const& test_expectation, T1 const& value1, T2 const& value2, T3 const& epsilon)
 	{
 		if constexpr (std::is_base_of_v<math::BasicMatrix, T1> && std::is_base_of_v<math::BasicMatrix, T2>)
 		{
 			if constexpr (value1.is_vector() && value2.is_vector())
-				detail::verify_equal_with_epsilon_vector (test_explanation, value1, value2, epsilon);
+				detail::verify_equal_with_epsilon_vector (test_expectation, value1, value2, epsilon);
 			else
-				detail::verify_equal_with_epsilon_matrix (test_explanation, value1, value2, epsilon);
+				detail::verify_equal_with_epsilon_matrix (test_expectation, value1, value2, epsilon);
 		}
 		else
-			detail::verify_equal_with_epsilon_scalar (test_explanation, value1, value2, epsilon);
+			detail::verify_equal_with_epsilon_scalar (test_expectation, value1, value2, epsilon);
 	}
 
 } // namespace neutrino::test_asserts
